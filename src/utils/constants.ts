a6aa54263b5865f2945878de675144074991a13c
lord-chooson <cusunliem1@gmail.com>
2021-04-15 13:24:51 +0700

    update

---
+++ src/utils/constants.ts
@@ -0,0 +1,54 @@
+export const PANCAKE_ROUTER = "0x05ff2b0db69458a0750badebc4f9e13add608c7f";
+export const BNB = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";
+export const BUSD = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
+export const USDT = "0x55d398326f99059ff775485246999027b3197955";
+export const AUTO = "0x0895196562C7868C5Be92459FaE7f877ED450452";
+export const ibBNB = "0xd7d069493685a581d27824fc46eda46b7efc0063";
+export const ibBUSD = "0x7c9e73d4c71dae564d41f78d56439bb4ba87592f";
+export const mebBUSD = "0x7e0c621ea9f7afd5b86a50b0942eaee68b04a61c";
+export const mebBNB = "0x639f18c72b6a017bdd209c161d1617c9481a1e4d";
+export const masterChefPresets = [
+  {
+    name: "-",
+    address: "",
+  },
+  {
+    name: "PancakeSwap",
+    address: "0x73feaa1ee314f8c655e354234017be2193c9e24e",
+  },
+  {
+    name: "ApeSwap",
+    address: "0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9",
+  },
+  {
+    name: "Goose",
+    address: "0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57",
+  },
+  {
+    name: "JAZZ",
+    address: "0xe09847360FbE8EBd8AF25BBd69937E100711883D",
+  },
+  {
+    name: 'Cheesecake',
+    address: "0x398d648c58ccf6337dded3dac7cbd7970ae474b8"
+  },
+  {
+    name: 'TAKO',
+    address: "0x4448336BA564bd620bE90d55078e397c26492a43"
+  },
+  {
+    name: 'PHY',
+    address: "0xa7A1A6f7A7f03295E209E3CB5C602a80E10049eC"
+  }
+];
+
+export const routerPresets = [
+  {
+    name: "PancakeSwap",
+    address: "0x05ff2b0db69458a0750badebc4f9e13add608c7f",
+  },
+  {
+    name: "ApeSwap",
+    address: "0xC0788A3aD43d79aa53B09c2EaCc313A787d1d607",
+  }
+];
